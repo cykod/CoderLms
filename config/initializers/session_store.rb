742edd91f128061221fe69d4df09ddeb94974197
Pascal Rettig <pascal@cykod.com>
2015-01-01 17:06:17 -0500

    Name Change

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_WebLms2_session'
+Rails.application.config.session_store :cookie_store, key: '_CoderLms_session'
