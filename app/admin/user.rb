235d0cda4603d0e0ec0beb41bb8bd1b69e115e2b
Pascal Rettig <pascal@cykod.com>
2015-05-13 21:15:29 -0400

    Clean up grading

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -39,17 +39,17 @@ LittleBigAdmin.model :user do
         end
         t.column :late
         t.column :validated do |ua|
-          ua.validation_errors.length > 0 ? true : false
-        end
-        t.column "Grade" do |ua|
-          render(partial: "/admin/user_grade", locals: { ua: ua })
+          ua.validation_errors.length > 0 ? "No"  : "Yes"
         end
         t.column "Homework" do |ua|
           if ua.lesson_assignment.requires_url?
-            link_to ua.url, ua.url, target: "_blank"
+            link_to ua.url.truncate(30), ua.url, target: "_blank"
           elsif ua.lesson_assignment.requires_file?
-            link_to ua.file.url, ua.file.url, target: "_blank"
+            link_to ua.file.url.truncate(30), ua.file.url, target: "_blank"
           end
+        end
+        t.column "Grade" do |ua|
+          render(partial: "/admin/user_grade", locals: { ua: ua })
         end
       end
     end
